0f80e3e7b7b487b339426375aa468ab5d9fc51fa
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-04-29 18:10:16 +0200

    install dependencies

--- Clients/Angular/src/app/app.module.ts
+++ Clients/Angular/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- Clients/Angular/src/app/sign-up/sign-up.module.ts
+++ Clients/Angular/src/app/sign-up/sign-up.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignupComponent } from './signup.component';
 
 
 
 @NgModule({
-  declarations: [
-    SignupComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule
   ]
 })
-export class SignupModule { }
+export class SignUpModule { }
